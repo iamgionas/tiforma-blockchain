5ee8bc09d0a58db4cc8bf825ce7491c9b8d02941
gion4s <gionas.bonardi@gmail.com>
2019-03-19 23:02:27 +0100

    Create a scaffolding of the front-end structure

---
+++ supsi-atc/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ supsi-atc/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentsComponent } from './students/students.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
