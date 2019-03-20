68d7cd290e88d5b59fecded3af54d83f0bbcc3b0
gion4s <gionas.bonardi@gmail.com>
2019-03-20 12:11:11 +0100

    Added the navbar

--- supsi-atc/src/app/app-routing.module.ts
+++ supsi-atc/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentsComponent } from './students/students.component';
+import { CoursesComponent } from './courses/courses.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/students', pathMatch: 'full' },
-  { path: 'students', component: StudentsComponent }
+  { path: 'students', component: StudentsComponent },
+  { path: 'courses', component: CoursesComponent }
 ];
 
 @NgModule({
--- supsi-atc/src/app/app.module.ts
+++ supsi-atc/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StudentsComponent } from './students/students.component';
+import { CoursesComponent } from './courses/courses.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentsComponent
+    StudentsComponent,
+    CoursesComponent,
+    NavbarComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
