ce6ad9d9f749b540e6bc6cd4262e8d8f9b4b7165
Brian Pulfer <brian.pulfer@student.supsi.ch>
2019-03-25 22:05:29 +0100

    Aggiunta del percorso 'courses' nel router. Aggiunta del dettaglio del corso

--- supsi-atc/src/app/courses/courses-detail/courses-detail.component.ts
+++ supsi-atc/src/app/courses/courses-detail/courses-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RestService } from 'src/app/rest.service';
 import { Route, ActivatedRoute, Router } from '@angular/router';
 
@@ -9,11 +9,25 @@ import { Route, ActivatedRoute, Router } from '@angular/router';
 })
 export class CoursesDetailComponent implements OnInit {
 
+  @Input() courseData: any = { courseCode: '', name: ''};
+
   constructor(private rest : RestService, private route : ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
-
+    this.route.params.subscribe((params : any) => {
+      if(params.id){
+        this.rest.getObject('Courses',params.id).subscribe((c) => this.course = c);
+      }
+    })
   }
 
-  
+  course : any;
+
+  modifyCourse(){
+    this.rest.updateObject('Courses', this.route.snapshot.params['id'], this.courseData).subscribe((result) => {
+      this.router.navigate([result.courseCode]);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
